0e445cdcb76def4042cb73c67434c44d7817fa15
Derick Gagnon <derick.gag46@hotmail.com>
2022-02-07 02:54:26 -0500

    StyleMD commenté

--- fichiersFournisTP1H22/TP/src/StyleMD.java
+++ fichiersFournisTP1H22/TP/src/StyleMD.java
@@ -1,10 +1,24 @@
+/**
+ * Classe abstraite gardant la disposition d'un texte et
+ * s'assurant que les méthodes qui l'implémentent aient une
+ * méthode formater. Peut aussi s'occuper de la préparation de
+ * texte et de sa disposition.
+ */
 public abstract class StyleMD {
+    //Constantes
     public final static int BLOC = 0;
     public final static int LIGNE = 1;
     private final String texteNull = "null";
 
+    //Attributs d'instances
     protected int disposition;
 
+    /**
+     * Initialise l'attribut d'instance disposition avec la valeur
+     * passée en paramètre. 1 pour bloc, 2 pour ligne. Tout autre
+     * valeur donnera une disposition bloc.
+     * @param disposition
+     */
     public StyleMD(int disposition) {
         if (disposition == BLOC || disposition == LIGNE)
             this.disposition = disposition;
@@ -12,13 +26,25 @@ public abstract class StyleMD {
             this.disposition = BLOC;
     }
 
-
+    /**
+     * Méthode abstraite qui retournera éventuellement le
+     * texte formaté
+     * @param texte le texte à formater
+     * @return le texte formaté
+     */
     public abstract String formater(String texte);
 
+    //Getter
     public int getDisposition() {
         return disposition;
     }
 
+    /**
+     * Redéfénition de la méthode equals de la classe Object.
+     * Est égal si la dispositino de l'autre style est la même
+     * @param autreStyle l'autre style à comparer
+     * @return Vrai si la disposition est la même
+     */
     public boolean equals(Object autreStyle) {
         if (autreStyle != null)
             if (autreStyle.getClass() == this.getClass())
@@ -26,6 +52,12 @@ public abstract class StyleMD {
         return super.equals(autreStyle);
     }
 
+    /**
+     * Arrang le texte pour être plus propre. S'il est null,
+     * retourne un String "null".
+     * @param texte Le texte à préparer
+     * @return le texte préparé
+     */
     protected String preparationTexte(String texte) {
         if (texte == null)
             texte = texteNull;
@@ -34,6 +66,12 @@ public abstract class StyleMD {
         return texte;
     }
 
+    /**
+     * Dispose le texte dépendant d'une dispositino pour un bloc
+     * ou une ligne.
+     * @param texte Le texte à arranger
+     * @return le texte arrangé
+     */
     protected String dispositionTexte(String texte) {
         if (disposition == BLOC)
             texte = "\n" + texte + "\n";
